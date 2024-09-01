0db6a53da30ac725a4f362e8aee00716309bc46b
yume190 <yume@linweijundeMacBook-Pro.local>
2024-09-01 21:07:54 +0800

    update patch

--- TestSwallow/patch.sh
+++ TestSwallow/patch.sh
@@ -9,10 +9,18 @@ do
   cd XCFrameworks/$name.xcframework/macos-arm64_x86_64/$name.framework
 
   mkdir -p Versions/A
-  cp -r $name Versions/A/$name
-  cp -r Headers Versions/A/Headers
-  cp -r Info.plist Versions/A/Info.plist
-  cp -r Modules Versions/A/Modules
+
+  mv $name Versions/A/$name
+  mv Info.plist Versions/A/Info.plist
+  mv Headers Versions/A/Headers
+  mv Modules Versions/A/Modules
+  
+  ln -s Versions/A/$name $name
+  ln -s Versions/A/Headers Headers
+  ln -s Versions/A/Info.plist Info.plist
+  ln -s Versions/A/Modules Modules
+
+  ln -s Versions/A Versions/Current
 
   cd -
 done
--- UseSwallow/Package.swift
+++ UseSwallow/Package.swift
@@ -29,7 +29,7 @@ var package = Package(
             swiftSettings: [
                 .unsafeFlags([
                    "-load-plugin-executable", "XCFrameworks/SwallowMacros#SwallowMacros"
-                ])
+                ]),
             ]
         ),
     ]
